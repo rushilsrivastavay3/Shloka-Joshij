b6578ac294cb411ebc0dea53df43eeedba7778b2
rushilsrivastavay3 <kcfcgxv0sidp@sina.com>
2021-01-19 05:04:19 +0800

    Card Component Added

--- src/shared/card.js
+++ src/shared/card.js
@@ -1,44 +1,24 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import React from "react";
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
-
-export default function BasicCard(props) {
+export default function BasicCardComponent(props) {
+  let a, img;
+  if (props.href) {
+    a = (
+      <a href={props.href} className="btn btn-outline-success">
+        {props.btnText}
+      </a>
+    );
+  } else if (props.imgSrc) {
+    img = <img src={props.imgSrc} alt="Image" />;
+  }
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          we{props.title1}
-        </Typography>
-        <Typography variant="h5" component="div">
-          {props.title2}
-          {bull}
-          {props.title3}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {props.title4}
-        </Typography>
-        <Typography variant="body2">
-          {props.title5}
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">More</Button>
-      </CardActions>
-    </Card>
+    <div className="card text-center">
+      <div className="overflow">{img}</div>
+      <div className="card-body text-dark">
+        <h4 className="class-title">{props.title}</h4>
+        <p className="card-text text-secondary">{props.text}</p>
+        {a}
+      </div>
+    </div>
   );
 }
