844833458882e3fb557c2d01199de608216c01e3
rushilsrivastavay3 <kcfcgxv0sidp@sina.com>
2021-01-21 00:30:04 +0800

    App.js Index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './components/login'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-fluid">
+        <div className="card shadow-lg  p-3 mb-5 bg-white rounded">
+          <div className="card-header">
+            <h4> App Components (Root)</h4>
+          </div>
+          <div className="card-body">
+           <Login/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
