d3bd9d94eefe10977fc38f71cb2032def17c81bb
rushilsrivastavay3 <kcfcgxv0sidp@sina.com>
2021-01-27 21:58:22 +0800

    first commit

---
+++ src/components/login.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Login extends React.Component{
+    render()
+    {
+     return(
+            <div className="container-fluid">
+                <div className="card">
+                    <div className="card-header">
+                        <h5>Login Page</h5>          
+                    </div>
+                    <div className="card-body">
+                    
+                    </div>
+                </div>
+            </div>
+       );
+    }
+
+}
+export default Login;
