b9a020a1d2b20696a3f1980923c171ef0aaec028
rushilsrivastavay3 <kcfcgxv0sidp@sina.com>
2021-02-24 11:28:33 +0800

    home page done by theja

---
+++ src/apiConstants.js
@@ -0,0 +1,15 @@
+const registerData = `http://localhost:9999/registerData`;
+
+const demographicsData = `http://localhost:9999/patientDemographics`;
+
+const medicationAndAllergiesTableData = `http://localhost:9999/medicationAndAllergies`;
+
+const immunization = `http://localhost:9999/immunization`;
+
+const scheduleAppointmentData = `http://localhost:9999/scheduleAppointment`;
+
+const patientReportsData = `http://localhost:9999/patientReports`;
+
+const billingData = `http://localhost:9999/billing`;
+
+const alleriesData = `http://localhost:9999/allergies`;
---
+++ src/shared/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import reactDom from "react-dom";
+
+function FooterComponent(){
+    return(
+        <div className="pt-2 card text-light bg-secondary">
+            <p>@copyrights 2020</p>
+        </div>
+    );
+}
+export default FooterComponent;
