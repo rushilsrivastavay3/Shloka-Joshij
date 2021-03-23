a515c801cb60ca410fc5f49f6cdbd694d8e718d7
rushilsrivastavay3 <kcfcgxv0sidp@sina.com>
2021-03-24 01:41:05 +0800

    temporary changes

--- src/shared/dashboard.js
+++ src/shared/dashboard.js
@@ -4,6 +4,7 @@ import BasicCardComponent from "./card";
 import HeaderComponent from "./header";
 import FooterComponent from "./footer";
 import home from "../images/home.png"
+import ProfileCardComponent from "./profileCard";
 
 // import {
 //   BrowserRouter as Router,
@@ -11,72 +12,58 @@ import home from "../images/home.png"
 //   Route,
 //   Link
 // } from "react-router-dom";
-function Dashboard() {
+function Dashboard(props) {
+    console.log(props);
     return (
         <div className="container-fluid">
 
             <HeaderComponent />
-                        
-                            {/* //Profile card */}
-                           
-                            <div className="row">
+
+            <div className="row">
 
                 <div className="col-md-3 card shadow-lg  bg-white rounded">
-                <div className="col-md-12 card ">
-                {/* <img   src={home}  alt="Profile" width="300"  /> */}
-                 <div><img   src={home}  alt="Profile"style={{width: '-webkit-fill-available'}}  /></div>
-               </div>
-               &nbsp;
-                <div className="col-md-12 ">
-                <button type="button" className="btn btn-primary w-100">Ok</button>
-               </div>
-               &nbsp;
-               <div className="col-md-12">
-               <button type="button" className="btn btn-primary w-100">Ok</button>
-               </div>
-               &nbsp;
-               
-</div>
+                    <div className="col-md-12 card ">
+                        {/* //Profile card */}
+                        <ProfileCardComponent cardData=""/>
+                        {/* <div><img src={home} alt="Profile" style={{ width: '-webkit-fill-available' }} /></div> */}
+                    </div>
+                    &nbsp;
+                    <div className="col-md-12 ">
+                      {/* {Menu card} */}
+                        
+                    </div>
+                    &nbsp;
 
-                    <div className=" col-sm-9 col-md-9 col-lg-9 col-xl-9">
-                        <div className="containerone">
-                            {/* //Main Content */}
+                </div>
 
-                            <div className="row reporttabel">
-                                <div className="col-md-6 ">                                   
-                                        <BasicCardComponent title="My Appointment" text="Antibiotics" href="#" btnText="More" />
-                                </div>
-                                {/* <div className="col-md-1 ">
-                                </div> */}
-                                <div className="col-md-6 ">
-                                    <BasicCardComponent title="Medication" text="Antibiotics" href="#" btnText="More" />
+                <div className=" col-sm-9 col-md-9 col-lg-9 col-xl-9">
+                    <div className="containerone">
+                        {/* //Main Content */}
 
-
-                                </div>
+                        <div className="row reporttabel">
+                            <div className="col-md-6 ">
+                                <BasicCardComponent title="My Appointment" text="Antibiotics" href="#" btnText="More" />
                             </div>
-
-                            <div className="row reporttabel">
-                                <div className="col-md-12  ">
-                                    <BasicCardComponent title="My reports" text="Table" href="#" btnText="More" />
-
-
-                                </div>
+                            {/* <div className="col-md-1 ">
+                                </div> */}
+                            <div className="col-md-6 ">
+                                <BasicCardComponent title="Medication" text="Antibiotics" href="#" btnText="More" />
 
                             </div>
                         </div>
 
+                        <div className="row reporttabel">
+                            <div className="col-md-12  ">
+                                <BasicCardComponent title="My reports" text="Table" href="#" btnText="More" />
 
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-
-           
+            </div>
             <FooterComponent />
         </div>
-
     );
-
-
 }
 
 export default Dashboard;
