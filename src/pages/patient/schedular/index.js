b8173c257f80100261cdf362cdd12f81268836fb
rushilsrivastavay3 <kcfcgxv0sidp@sina.com>
2021-04-23 23:55:07 +0800

    home-page responsiveness-completed

---
+++ src/pages/patient/schedular/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import reactDom from "react-dom";
+
+function ScheduleAppointment()
+{
+    return(
+        <>
+            <h1>Schedule an new Appointment</h1>
+        </>
+    );
+}
+
+export default ScheduleAppointment;
